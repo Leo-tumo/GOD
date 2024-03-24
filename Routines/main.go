6fab7752dba0601e3c2df1923eceafd30d8fd69a
leo <leo.tumanyan@gmail.com>
2024-03-24 17:02:29 +0400

    go interfaces, Stringer

---
+++ Routines/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	l := list{
+		{title: "moby dick", price: 10, released: toTimestamp(118281600)},
+		{title: "odyssey", price: 15, released: toTimestamp("733622400")},
+		{title: "hobbit", price: 25},
+		//{title: "rubik's cube", price: 5},
+		//{title: "minecraft", price: 20},
+		//{title: "tetris", price: 5},
+		//{title: "yoda", price: 150},
+	}
+
+	l.discount(.5)
+	fmt.Print(l)
+
+}
